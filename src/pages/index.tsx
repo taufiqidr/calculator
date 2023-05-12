2573438c4a31d43d283a656a6e550b3ec710e6d1
Taufiqurrahman Idrus <taufiqmck@gmail.com>
2023-05-12 13:47:20 +0800

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { type NextPage } from "next";
+import Sanity from "./sanity";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Sanity></Sanity>
+    </>
+  );
+};
+
+export default Home;
